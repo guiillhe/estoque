609593b2ec96aec8c6aa12b424165b6d3b89405a
guiillhe <gja.gja@gmail.com>
2026-01-29 11:02:29 +0000

    ajustando o cors

--- inventory/settings.py
+++ inventory/settings.py
@@ -30,6 +30,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://estoque.guilhermearaujo.cloud',
+    'https://www.estoque.guilhermearaujo.cloud',
+]
 
 # Application definition
 
